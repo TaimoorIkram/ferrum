6ebe6baf6dbb66bcda64e4dbac5022aa94b58e44
TaimoorIkram <tikram.bese21seecs@seecs.edu.pk>
2025-12-12 15:29:05 +0500

    define the persistence library to store database core

---
+++ src/persistence/mod.rs
@@ -0,0 +1,5 @@
+//! Persistence as tables needs to have the following components
+//! - Schema (mapping of column names to datatypes allowed, order is important)
+//! - Row (based on a Schema, fixed per table, only one write and many reads)
+//! - Table (made of many Rows, multi-threadable)
+//!
