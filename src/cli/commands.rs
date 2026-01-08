ef0a2a03bda7575ab5eeef69d2db35f0146e682e
TaimoorIkram <tikram.bese21seecs@seecs.edu.pk>
2026-01-08 19:36:27 +0500

    implement basic insert into table query

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -17,19 +17,18 @@
 //!
 //! Here * means more than one such values separated by a comma.
 
-use std::collections::HashMap;
 use std::fmt::Display;
 use std::sync::{Arc, RwLock};
 use std::vec;
 
 use indexmap::IndexMap;
 use sqlparser::ast::{
-    ColumnDef, ColumnOption, DataType, ObjectName, Select, SelectItem, SetExpr, Statement,
-    TableConstraint, TableFactor,
+    ColumnDef, ColumnOption, DataType, Expr, ObjectName, Select, SelectItem, SetExpr, Statement,
+    TableConstraint, TableFactor, TableObject, Value, ValueWithSpan,
 };
 
-use crate::cli::messages::system_message;
-use crate::persistence::{Database, Row, Table, TableReader};
+use crate::cli::messages::{highlight_argument, system_message};
+use crate::persistence::{Database, Row, TableReader};
 
 /// The executor class that runs the statements.
 ///
@@ -234,6 +233,84 @@ impl SqlExecutor {
         }
     }
 
+    fn _parse_value_to_engine(&self, value: ValueWithSpan) -> Result<(String, String), String> {
+        //! Match the [`Value`] object properly to its subtype and return the
+        //! engine-specific (value, datatype) mapping.
+        //!
+        //! This function is a utility to allow data insertion format to align
+        //! with the one accepted by the persistence API
+
+        let value = match value.value {
+            Value::Number(value, _) => (value, "num".to_string()),
+            Value::SingleQuotedString(value) => (value, "txt".to_string()),
+            Value::DoubleQuotedString(value) => (value, "txt".to_string()),
+            _ => {
+                return Err(system_message(
+                    "system",
+                    format!(
+                        "Unsupported value: {}",
+                        highlight_argument(&value.to_string())
+                    ),
+                ));
+            }
+        };
+
+        Ok(value)
+    }
+
+    fn _extract_row(&self, values: Vec<Expr>) -> Result<Vec<String>, String> {
+        //! Extract a row form a [`Vec<Expr>`] to create a [`Vec<String>`]
+        //! which is compatible with the persistence api.
+        //!
+        //! Internally, uses an extraction function to extract all supported
+        //! expression types into a String that can be stored inside the engine
+        //!
+        //! # Issues
+        //! - This function is not recursive, which it should be; it should be able
+        //! to call recursively itself, untul a [`ValueWithSpan`] is obtained, that
+        //! can then be parsed using the [`self._parse_value_to_engine`] function.
+
+        let mut row = vec![];
+
+        for value in values {
+            match value {
+                Expr::Value(value) => row.push(self._parse_value_to_engine(value)?.0),
+                Expr::UnaryOp { op, expr } => {
+                    let value_with_span = match expr.as_ref() {
+                        Expr::Value(value) => value.clone(),
+                        _ => {
+                            return Err(system_message("system", "Unsupported value.".to_string()));
+                        }
+                    };
+
+                    let sign = match op {
+                        sqlparser::ast::UnaryOperator::Minus => "-",
+                        _ => {
+                            return Err(system_message(
+                                "system",
+                                "Unsupported unary operator.".to_string(),
+                            ));
+                        }
+                    };
+
+                    let final_value = vec![
+                        sign.to_string(),
+                        self._parse_value_to_engine(value_with_span)?.0,
+                    ];
+                    row.push(final_value.join(""));
+                }
+                _ => {
+                    return Err(system_message(
+                        "system",
+                        "Unsupported value. Check your query.".to_string(),
+                    ));
+                }
+            }
+        }
+
+        Ok(row)
+    }
+
     pub fn new(statement: Statement, database: &Arc<RwLock<Database>>) -> SqlExecutor {
         SqlExecutor {
             statement,
@@ -294,6 +371,53 @@ impl SqlExecutor {
                     "This type of query is not handled by the engine yet!".to_string(),
                 )),
             },
+            Statement::Insert(insert) => {
+                // check if the table exists in the database
+                // process the rows one by one to convert them to persistence api
+                // compatible rows
+                // processed row is inserted into the table
+
+                let table_name = match insert.table.clone() {
+                    TableObject::TableName(obj) => obj.0[0].as_ident().unwrap().value.clone(),
+                    _ => return Err("Invalid table name. Please check your query.".to_string()),
+                };
+
+                let mut database = self.database.write().unwrap();
+                if database.contains_table(&table_name) {
+                    let query_body = insert.source.clone().expect(&system_message(
+                        "system",
+                        "No values to insert.".to_string(),
+                    ));
+
+                    let query_rows = match query_body.body.as_ref() {
+                        SetExpr::Values(values) => values.rows.clone(),
+                        _ => {
+                            return Err("Invalid values list. Please check your query.".to_string());
+                        }
+                    };
+
+                    let mut rows = vec![];
+
+                    for row in query_rows {
+                        // extract a row and add to a list of rows
+                        // use the insert many to insert the rows together
+                        rows.push(self._extract_row(row)?);
+                    }
+
+                    let inserted_row_count = database.insert_many_into_table(&table_name, rows)?;
+
+                    Ok(SqlResult {
+                        table: None,
+                        row: None,
+                        n_rows_processed: Some(inserted_row_count),
+                    })
+                } else {
+                    return Err(system_message(
+                        "system",
+                        format!("Table {} does not exist.", highlight_argument(&table_name)),
+                    ));
+                }
+            },
             Statement::CreateTable(create_table) => {
                 let table_name = create_table
                     .name
@@ -334,9 +458,9 @@ impl SqlExecutor {
 
                 let mut database = self.database.write().unwrap();
                 let column_definitions = col_def_map
-                        .values()
-                        .into_iter()
-                        .map(|def| def.join(" "))
+                    .values()
+                    .into_iter()
+                    .map(|def| def.join(" "))
                     .collect();
 
                 database.create_table(table_name, column_definitions)?;
